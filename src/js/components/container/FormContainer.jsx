f017e18507bea5ab4150d2a93732e950393b29d0
Duke Goulden <dukegoulden@gmail.com>
2019-02-12 17:03:45 -0600

    first push

---
+++ src/js/components/container/FormContainer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+class FormContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: ''
+    };
+  }
+  render() {
+    return <form id="article-form" />;
+  }
+}
+export default FormContainer;
